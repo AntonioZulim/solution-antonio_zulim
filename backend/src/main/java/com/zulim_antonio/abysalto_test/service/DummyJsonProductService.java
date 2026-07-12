e332e951ccea2bef4f8aa1db0476691e4b26ff8c
AntonioZulim <antonio.zulim@gmail.com>
2026-07-12 23:37:43 +0200

    Refactored service - replaced with ProductService interface - old class renamed and implements ProductService

---
+++ backend/src/main/java/com/zulim_antonio/abysalto_test/service/DummyJsonProductService.java
@@ -0,0 +1,74 @@
+package com.zulim_antonio.abysalto_test.service;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.zulim_antonio.abysalto_test.client.ProductClient;
+import com.zulim_antonio.abysalto_test.dto.CategoryDto;
+import com.zulim_antonio.abysalto_test.dto.ProductDto;
+import com.zulim_antonio.abysalto_test.dto.ProductSummaryDto;
+
+@Service
+public class DummyJsonProductService implements ProductService{
+    private static final Logger log = LoggerFactory.getLogger(DummyJsonProductService.class);
+    private final ProductClient productClient;
+
+    public DummyJsonProductService(ProductClient productClient) {
+        this.productClient = productClient;
+    }
+   
+    @Cacheable(value = "products", key = "#id")
+    public ProductDto getProductById(Long id){
+        return productClient.getProduct(id);
+    }
+
+    @Cacheable(value = "products", key = "#category + '-' + #minPrice + '-' + #maxPrice", unless = "#result == null || #result.isEmpty()")
+    public Optional<List<ProductSummaryDto>> getProducts(String category, Double minPrice, Double maxPrice){
+        log.info("Fetching products with filters - category: {}, minPrice: {}, maxPrice: {}", category, minPrice, maxPrice);
+
+        Stream<ProductDto> prods;
+        if(category!=null){
+            Optional<CategoryDto> foundCategory = productClient.getCategories().stream().filter(c -> c.slug().equals(category)).findFirst();
+            if(!foundCategory.isPresent()){
+                log.warn("Category '{}' not found", category);
+                return Optional.empty();
+            }
+            prods = productClient.getProductsByAbsoluteUrl(URI.create(foundCategory.get().url())).products().stream();
+        }
+        else{
+            prods = productClient.getAllProducts().products().stream();
+        }
+
+        if(minPrice!=null){
+            prods = prods.filter(p -> p.price()>=minPrice);
+        }
+        if(maxPrice!=null){
+            prods = prods.filter(p -> p.price()<=maxPrice);
+        }
+        
+        List<ProductSummaryDto> res = prods.map(p -> new ProductSummaryDto(p.thumbnail(), p.title(), p.price(), shorten(p.description()))).toList();
+        log.info("Fetched {} products", res.size());
+        return Optional.of(res);
+    }
+
+    @Cacheable(value = "products", key = "#name")
+    public List<ProductSummaryDto> searchProducts(String name){
+        return productClient.getAllProducts()
+                            .products()
+                            .stream()
+                            .filter(p -> p.title().toLowerCase().contains(name.toLowerCase()))
+                            .map(p -> new ProductSummaryDto(p.thumbnail(), p.title(), p.price(), shorten(p.description())))
+                            .toList();
+    }
+
+    private String shorten(String desc){
+        return desc.length()>100 ? desc.substring(0, 100) + "..." : desc;
+    }
+}
